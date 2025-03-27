5b0aecc74f786464085e235a6edd276be3f3ba18
SquishieInc <squishie.inc@gmail.com>
2025-03-27 12:08:58 +0000

    Update BusinessController.cs

--- BusinessController.cs
+++ BusinessController.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class BusinessController : MonoBehaviour
+public class BusinessController : MonoBehaviour, IPrestigeable
 {
     public BusinessSO businessData;
     public int level = 0;
@@ -25,11 +25,26 @@ public class BusinessController : MonoBehaviour
     {
         if (level > 0)
         {
-            double income = businessData.baseIncome * level;
-            CurrencyManager.Instance.AddCash(income);
+            double baseIncome = businessData.baseIncome * level;
+            double multiplier = 1.0 + (0.1 * PrestigeManager.Instance.prestigePoints); // +10% per point
+            double finalIncome = baseIncome * multiplier;
+
+            CurrencyManager.Instance.AddCash(finalIncome);
         }
     }
 
     public double GetCurrentCost() => currentCost;
-    public double GetIncomePerCycle() => businessData.baseIncome * level;
+    public double GetIncomePerCycle()
+    {
+        double baseIncome = businessData.baseIncome * level;
+        double multiplier = 1.0 + (0.1 * PrestigeManager.Instance.prestigePoints);
+        return baseIncome * multiplier;
+    }
+
+    // 🧼 Reset logic for PrestigeManager
+    public void OnPrestigeReset()
+    {
+        level = 0;
+        currentCost = businessData.baseCost;
+    }
 }
