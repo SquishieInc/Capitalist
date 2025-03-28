2d161021ec5cf4459fd3f3476f353129021d0371
SquishieInc <squishie.inc@gmail.com>
2025-03-28 14:09:49 +0000

    Update GameConfigDebugPanel.cs

--- GameConfigDebugPanel.cs
+++ GameConfigDebugPanel.cs
@@ -1,57 +1,91 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections.Generic;
 
 public class GameConfigDebugPanel : MonoBehaviour
 {
     [Header("References")]
+    public GameObject panelRoot;
     public GameBalanceConfigSO config;
 
     [Header("UI Fields")]
     public TMP_InputField prestigeDivisorInput;
     public TMP_InputField offlineRateInput;
     public TMP_InputField cheatPenaltyInput;
-
     public TMP_InputField hireManagerMultiplierInput;
     public TMP_InputField costMultiplierInput;
 
     public Button applyButton;
     public TMP_Text statusText;
 
+    [Header("Config Presets")]
+    public GameBalanceConfigSO[] profilePresets;
+    public TMP_Dropdown profileDropdown;
+
     private void Start()
     {
         LoadConfigValues();
+        BuildProfileDropdown();
+
         applyButton.onClick.AddListener(ApplyChanges);
+        profileDropdown.onValueChanged.AddListener(LoadProfileByIndex);
+    }
+
+    private void Update()
+    {
+#if UNITY_EDITOR
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            panelRoot.SetActive(!panelRoot.activeSelf);
+        }
+#endif
     }
 
     private void LoadConfigValues()
     {
+        if (GameConfigManager.Instance.Config == null) return;
+
+        config = GameConfigManager.Instance.Config;
+
         prestigeDivisorInput.text = config.prestigeBaseDivisor.ToString();
         offlineRateInput.text = config.baseOfflineEarningsRate.ToString();
         cheatPenaltyInput.text = config.timeCheatPenaltyMultiplier.ToString();
-
         hireManagerMultiplierInput.text = config.hireManagerCostMultiplier.ToString();
         costMultiplierInput.text = config.defaultCostMultiplier.ToString();
     }
 
     private void ApplyChanges()
     {
-        if (double.TryParse(prestigeDivisorInput.text, out double pd))
-            config.prestigeBaseDivisor = pd;
+        if (config == null) return;
 
-        if (float.TryParse(offlineRateInput.text, out float or))
-            config.baseOfflineEarningsRate = or;
+        double.TryParse(prestigeDivisorInput.text, out config.prestigeBaseDivisor);
+        float.TryParse(offlineRateInput.text, out config.baseOfflineEarningsRate);
+        float.TryParse(cheatPenaltyInput.text, out config.timeCheatPenaltyMultiplier);
+        float.TryParse(hireManagerMultiplierInput.text, out config.hireManagerCostMultiplier);
+        float.TryParse(costMultiplierInput.text, out config.defaultCostMultiplier);
 
-        if (float.TryParse(cheatPenaltyInput.text, out float cp))
-            config.timeCheatPenaltyMultiplier = cp;
+        GameConfigManager.Instance.NotifyConfigUpdated();
+        statusText.text = "✅ Config applied!";
+    }
 
-        if (float.TryParse(hireManagerMultiplierInput.text, out float hm))
-            config.hireManagerCostMultiplier = hm;
+    private void BuildProfileDropdown()
+    {
+        profileDropdown.ClearOptions();
+        List<string> names = new List<string>();
+        foreach (var preset in profilePresets)
+        {
+            names.Add(preset.name);
+        }
+        profileDropdown.AddOptions(names);
+    }
 
-        if (float.TryParse(costMultiplierInput.text, out float cm))
-            config.defaultCostMultiplier = cm;
-
-        statusText.text = "✅ Config updated at runtime!";
-        Debug.Log("[Debug Panel] Config updated!");
+    private void LoadProfileByIndex(int index)
+    {
+        if (index >= 0 && index < profilePresets.Length)
+        {
+            GameConfigManager.Instance.Config = profilePresets[index];
+            LoadConfigValues();
+        }
     }
 }
