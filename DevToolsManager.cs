735f810a0f37547b2b5061f0c6f82ed91770a59a
SquishieInc <squishie.inc@gmail.com>
2025-03-28 15:54:36 +0000

    Update DevToolsManager.cs

--- DevToolsManager.cs
+++ DevToolsManager.cs
@@ -5,38 +5,44 @@ public class DevToolsManager : MonoBehaviour
     public static DevToolsManager Instance;
 
     [Header("Tool Toggles")]
-    public GameObject analyticsOverlay;
     public GameObject configDebugPanel;
+    public GameObject analyticsOverlay;
+    public GameObject cheatPanel;
 
     private void Awake()
     {
-        if (Instance == null)
-        {
-            Instance = this;
-        }
+        if (Instance == null) Instance = this;
 
-        // Hide if not a debug build
-        if (!Debug.isDebugBuild)
-        {
-            analyticsOverlay.SetActive(false);
-            configDebugPanel.SetActive(false);
-            return;
-        }
-
-        analyticsOverlay.SetActive(true);
-        configDebugPanel.SetActive(true);
+        // Hide everything if not in a debug/development build
+#if !UNITY_EDITOR && !DEVELOPMENT_BUILD
+        if (configDebugPanel) configDebugPanel.SetActive(false);
+        if (analyticsOverlay) analyticsOverlay.SetActive(false);
+        if (cheatPanel) cheatPanel.SetActive(false);
+        enabled = false;
+#else
+        if (configDebugPanel) configDebugPanel.SetActive(false);
+        if (analyticsOverlay) analyticsOverlay.SetActive(false);
+        if (cheatPanel) cheatPanel.SetActive(false);
+#endif
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F1))
+#if UNITY_EDITOR || DEVELOPMENT_BUILD
+        if (Input.GetKeyDown(KeyCode.F1) && configDebugPanel != null)
         {
             configDebugPanel.SetActive(!configDebugPanel.activeSelf);
         }
 
-        if (Input.GetKeyDown(KeyCode.F2))
+        if (Input.GetKeyDown(KeyCode.F2) && analyticsOverlay != null)
         {
             analyticsOverlay.SetActive(!analyticsOverlay.activeSelf);
         }
+
+        if (Input.GetKeyDown(KeyCode.F3) && cheatPanel != null)
+        {
+            cheatPanel.SetActive(!cheatPanel.activeSelf);
+        }
+#endif
     }
 }
