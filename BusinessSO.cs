3396ef3fc29eed6d6186875d6b9327d0aea5042b
SquishieInc <squishie.inc@gmail.com>
2025-03-28 10:24:05 +0000

    Update BusinessSO.cs

--- BusinessSO.cs
+++ BusinessSO.cs
@@ -3,9 +3,13 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewBusiness", menuName = "Idle/Business")]
 public class BusinessSO : ScriptableObject
 {
+    [Header("Basic Info")]
     public string businessName;
     public double baseCost;
     public double baseIncome;
     public float incomeInterval = 1f;
-    public float upgradeCostMultiplier = 1.15f;
+
+    [Header("Scaling")]
+    public float costMultiplier = 1.15f;      // Cost increase per level
+    public float incomeGrowth = 1.0f;         // Exponential income scaling
 }
